832b7eef327c96f082cfc01542a536747d532bac
Miracle <cao4618077@126.com>
2017-09-26 21:39:01 +0800

    add recycle view

--- app/src/main/java/com/stories/healthy/fragments/MomentItem.java
+++ app/src/main/java/com/stories/healthy/fragments/MomentItem.java
@@ -3,6 +3,7 @@ package com.stories.healthy.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,10 +15,15 @@ import com.stories.healthy.R;
  */
 
 public class MomentItem extends Fragment {
+
+    private RecyclerView mRecyclerView;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_moment_item, container, false);
+
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.moment_item_recycle_view);
 
         return view;
     }
